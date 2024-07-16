642dbd5ef0935da409886c2989f628b83932eaa9
Mark Nyberg <bigusdeveloper@gmail.com>
2024-07-16 07:06:27 +0200

    Added some computed values to top of page.

--- littlefield_dashboard.py
+++ littlefield_dashboard.py
@@ -15,31 +15,54 @@ with data:
         all_data = pd.read_excel(uploaded_file, index_col=0)
         text_data = pd.read_excel(uploaded_file, sheet_name="Text Data", index_col=0)
         transaction_data = pd.read_excel(uploaded_file, sheet_name="Transaction History")
+        inventory_data = pd.read_excel(uploaded_file, sheet_name="Inventory Data", index_col=0)
 
         day_value = text_data.loc["Day", "Value"]
         balance_value = text_data.loc["Balance", "Value"]
 
-        st.markdown(f"**Day:** {day_value}")
-        st.markdown(f"**Balance:** ${balance_value}")
-        st.markdown(f"**{text_data.loc["Order Status", "Value"]}**")
+        with st.container():
+            left_column, right_column = st.columns(2)
+
+            start_day = left_column.number_input("Start day", min_value=all_data.index[0], max_value=all_data.index[-1],
+                                                 value=all_data.index[0])
+            end_day = right_column.number_input("End day", min_value=all_data.index[0], max_value=all_data.index[-1],
+                                                value=all_data.index[-1])
+
+            day_range = [start_day, end_day]
+            inventory_range = [start_day, end_day + 0.5]
+
+        KITS_IN_LOT = 60
+
+        total_wip = (all_data.iloc[-1, 3] + all_data.iloc[-1, 4] + all_data.iloc[-1, 5]) / KITS_IN_LOT
+
+        st.divider()
+
+        with st.container():
+            left_column, left_middle_column, right_middle_column, right_column = st.columns(4)
+
+            left_column.markdown(f"**Day:** {day_value}")
+            left_column.markdown(f"**Balance:** ${balance_value}")
+            left_middle_column.markdown(f"**Inventory level:** {inventory_data.iloc[-1, 1] / 60:.0f}")
+            left_middle_column.markdown(f"**Total WIP:** {total_wip:.0f}")
+            right_middle_column.markdown(
+                f"**Last 24 days mean inbound jobs:** {all_data[-24:]['Daily accepted kits'].mean():.1f}")
+            right_middle_column.markdown(
+                f"**Last 24 days mean completed jobs:** {all_data[-24:]['Daily Completed Jobs - Seven day'].mean():.1f}")
+
+            right_column.markdown(f"**{text_data.loc["Order Status", "Value"]}**")
+
+        if all_data[-1:]["Jobs Waiting Kits"].values > 0:
+            st.markdown(f":red[**Jobs waiting kits:** {all_data[-1:]['Jobs Waiting Kits'].values[0]}]")
+
+        st.divider()
 
         left_column, right_column = st.columns(2)
-
-        start_day = left_column.number_input("Start day", min_value=all_data.index[0], max_value=all_data.index[-1],
-                                             value=all_data.index[0])
-        end_day = right_column.number_input("End day", min_value=all_data.index[0], max_value=all_data.index[-1],
-                                            value=all_data.index[-1])
-
-        day_range = [start_day, end_day]
-        inventory_range = [start_day, end_day + 0.5]
 
         accepted_kits_fig = px.line(all_data, x=all_data.index, y=["Daily accepted kits"],
                                     title="Accepted kits", range_x=day_range)
         left_column.plotly_chart(accepted_kits_fig)
 
-        inventory_df = pd.read_excel(uploaded_file, sheet_name="Inventory Data", index_col=0)
-
-        inventory_level_fig = px.line(inventory_df, x="day", y="data",
+        inventory_level_fig = px.line(inventory_data, x="day", y="data",
                                       title="Inventory Level", range_x=inventory_range)
         right_column.plotly_chart(inventory_level_fig)
 
@@ -72,8 +95,10 @@ with data:
                                    range_x=day_range)
         right_column.plotly_chart(waiting_kits_fig)
 
-        avg_rev_per_job_fig = px.line(all_data, x=all_data.index, y=["Avg Rev per Job  - Seven day", "Avg Rev per Job  - One day", "Avg Rev per Job  - Half day"], title="Average Revenue per Job",
-                                   range_x=day_range)
+        avg_rev_per_job_fig = px.line(all_data, x=all_data.index,
+                                      y=["Avg Rev per Job  - Seven day", "Avg Rev per Job  - One day",
+                                         "Avg Rev per Job  - Half day"], title="Average Revenue per Job",
+                                      range_x=day_range)
         left_column.plotly_chart(avg_rev_per_job_fig)
 
         right_column.markdown("**Transaction history**")
@@ -83,7 +108,7 @@ with data:
         left_column.dataframe(
             all_data.loc[start_day:end_day,
             ["Daily accepted kits", "Daily Completed Jobs - Seven day", "Daily Completed Jobs - One day",
-             "Daily Completed Jobs - Half day"]].describe())
+             "Daily Completed Jobs - Half day"]].describe().loc[['count', 'mean', 'std', 'min', 'max']], )
 
         left_column.markdown("*Stats based on day range selected*")
 
